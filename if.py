26207451e61c4ee24341faa11aa2e7201aa9d235
chai2543 <52267052+chai2543@users.noreply.github.com>
2019-09-13 17:02:22 +0700

    Add files via upload

---
+++ if.py
@@ -0,0 +1,7 @@
+num	=	int(	input(	"Please	Enter	A	Number:	"	)	)
+if	num	>	5	:														
+      print(	"Number	Exceeds	5"	) 
+elif	num	<	5	:														
+      print(	"Number	is	Less	Than	5"	) 
+else	:														
+print(	"Number	Is	5"	) 
---
+++ skip.py
@@ -0,0 +1,17 @@
+bool	=	True 
+if	bool	:												
+print(	"Python	In	Easy	Steps"	) 
+else	:												
+#	Statements	to	be	inserted	here.
+title	=	"\nPython	In	Easy	Steps\n"
+for	char	in	title	:	print(	char	,	end	=	"	"	) 
+for	char	in	title	:												
+    if	char	==	"y"	:																				
+           print(	"*"	,	end	=	"	"	)																				
+           continue												
+    print(	char	,	end	=	"	"	) 
+for	char	in	title	:												
+        if	char	==	"y"	:																				
+                print(	"*"	,	end	=	"	"	)																				
+                pass												
+         print(	char	,	end	=	"	"	) 
